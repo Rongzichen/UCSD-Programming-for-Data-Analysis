0db623e67e2726663c5996f2cb1e4060ea8cfa10
Qingyang Su <Ken@Qingyangs-MBP-3.san.rr.com>
2019-12-06 23:51:36 -0800

    add main function to py file

--- proj_script.py
+++ proj_script.py
@@ -9,6 +9,10 @@ import plotly.graph_objs as go
 import math
 import addfips
 import xlrd
+import plotly.figure_factory as ff
+import geopandas
+import pandas as pd
+from wordcloud import WordCloud
 
 us_state_abbrev = {
     'Alabama': 'AL',
@@ -530,6 +534,296 @@ def getFIPS(city_tuple, values):
 
 
 
+if __name__ == '__main__':
+    """
+    This function shows the skills that UCSD Alumni possessed
+    """
+    #=============== Set up the data ===================#
+    skill, number = seperateData(skills)
+    number = [number[i]/sum(number) for i in range(len(number))] #convert number to percentage
+    skill, number = getHighest(skill, number, 10)  # Only display the top NO.15 skills
+    total = sum(number)
+    number = [i/total for i in number]
+    figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
+    colors=['gold', 'lightcoral','lightskyblue','lightcyan',  
+            'pink', 'lightgreen', 'plum', 'lightgrey', 'lightblue',
+           'limegreen','coral','c','dodgerblue','tomato','lightsalmon']
+    plt.rcParams.update({'font.size': 20})
+    plt.pie(number, labels=skill, colors=colors, shadow=False)
+    plt.title("Top 10 Skills UCSD alumni mastered")
+    plt.savefig('Top 15 Skills')
 
 
+    #=============== Set up the data ===================#
+    company, number = seperateDataCompany(companies)
+    company, number = getHighest(company, number, 12)  # Only display the top NO.15 companies
+    company, number = getSorted(company, number)
+    #============== Set up the Figures =================#
+    of.offline.init_notebook_mode(connected=True)
+    trace = [go.Bar(
+        x = company,
+        y = number,
+        orientation='v',
+        text = number,
+#         textposition = 'auto',
+        marker_color='rgb(255, 125, 102)'
+    )]
+    layout = go.Layout(plot_bgcolor='#fff', title =" Top 10 Companies UCSD alumni went to", titlefont=dict(color='rgb(200, 100, 100)',size=30))
+    data = [trace]
+    fig = go.Figure(data = trace, layout = layout)
+    fig.update_layout(    font=dict(
+        size=12,
+        color="#7f7f7f"
+    ))
+    of.iplot(fig)
+
+
+
+    """
+    This function is used to get the radar plot of the industry that alumni went to
+    """
+    
+    #=============== Set up the data ===================#
+    industries, number = seperateData(industry)
+    industries, number = getHighest(industries, number, 5)  # Only display the top NO.15 industries
+    
+    
+    industries = ['computer software + internet', \
+                 'semiconductors + electonic + networking',\
+                 'information technology & services',\
+                 'research',\
+                 'consulting + marketing + banking'\
+                 ]
+    number = [industryName['computer software'] + industryName['internet'],\
+             industryName['semiconductors'] + industryName['consumer electronics'] + industryName['computer hardware'] +\
+            industryName['computer networking'] + industryName['electrical & electronic manufacturing'] + \
+            industryName['wireless'] + industryName['telecommunications'],\
+              industryName['information technology & services'],\
+             industryName['research'],\
+             industryName['management consulting'] + industryName['financial services'] + \
+        industryName['marketing & advertising'] + industryName['investment banking/venture'] + \
+        industryName['banking'] + industryName['venture capital']]
+    industries, number = log_the_value(industries, number)
+    #============== Set up the Figures =================#
+    fig = go.Figure()
+    fig.add_trace(go.Scatterpolar(
+      r=number,
+      theta=industries,
+      fill='toself',
+      name='Industry that UCSD Alumni went to',
+      opacity  = 0.5, # Set up the opacity of the figure
+      marker_color='rgb(177, 66, 255)' #Adjust the color of the covered area
+    ))
+
+    fig.update_layout(
+    plot_bgcolor='#fff',
+    polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 8]
+    )),
+    font=dict(
+#         family="Courier New, monospace",
+        size=20
+#         color="#7f7f7f"
+    ),
+    title =" Top 5 Industry UCSD alumni went to", 
+    titlefont = dict(color='rgb(200, 100, 100)',size=30),
+    showlegend=False
+    )
+    
+    fig.show()
+
+
+
+
+    """
+    This is the plot for the majors
+    """
+    #=============== Set up the data ===================#
+    industries, number = seperateData(field_of_study)
+#     print(industries)
+    industries, number = getHighest(industries, number, 5)  # Only display the top NO.15 industries
+    industries, number = log_the_value(industries, number)
+    #============== Set up the Figures =================#
+    fig = go.Figure()
+    fig.add_trace(go.Scatterpolar(
+      r=number,
+      theta=industries,
+      fill='toself',
+      name='Major that UCSD Alumni studied in',
+      opacity  = 0.5, # Set up the opacity of the figure
+      marker_color='rgb(177, 66, 255)' #Adjust the color of the covered area
+    ))
+
+    fig.update_layout(
+    plot_bgcolor='#fff',
+    polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 8]
+    )),
+    font=dict(
+#         family="Courier New, monospace",
+        size=13.2
+#         color="#7f7f7f"
+    ),
+    title =" Top 5 Major UCSD alumni studied", 
+    titlefont = dict(color='rgb(200, 100, 100)',size=30),
+    showlegend=False
+    )
+    
+    fig.show()
+
+
+    """
+    This function is used to display the location that UCSD Alumni distributed
+    """
+
+    loc, value = seperate_Loc_Data(location)
+    loc, value = coordinate_Loc_Val(loc, value)
+    city, value = seperate_City_State_Data(location)
+    fips, value, cityName = getFIPS(city, value)
+
+    scope = ['California']
+    values = range(len(fips))
+    _, value = log_the_value(cityName, value)
+
+    colorscale = [
+        'rgb(255, 0, 0)',
+        'rgb(255, 30, 0)',
+        'rgb(255, 60, 0)',
+        'rgb(255, 90, 0)',
+        'rgb(255, 120, 0)',
+        'rgb(255, 180, 0)',
+        'rgb(255, 210, 0)',
+        'rgb(255, 240, 0)',
+        'rgb(255, 255, 160)'
+    ]
+
+    colorscale.reverse()
+
+    fig = ff.create_choropleth(fips=fips, values=value, colorscale = colorscale, scope=['CA'],county_outline={'color': 'rgb(155,155,155)','width': 1}, state_outline={'color': 'rgb(0, 0, 0)','width': 1},
+                               round_legend_values=True)
+    fig.layout.template = None
+
+    fig.update_layout(title =" Top 5 Industry UCSD alumni went to", 
+        titlefont=dict(color='rgb(200, 100, 100)',size=30),
+        geo = dict(showframe=False,
+            showcoastlines=True,
+            showland=False,
+            landcolor="lightgray"))
+    fig.show()
+
+
+    # df = pd.read_csv('2011_us_ag_exports.csv')
+    loc, value = seperate_Loc_Data(location)
+    loc, value = coordinate_Loc_Val(loc, value)
+    line = plotly.graph_objects.choropleth.marker.Line()
+
+    fig = go.Figure(data=go.Choropleth(
+        locations=loc, # Spatial coordinates
+        z = value, # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        marker_line_color = 'black', # display the color of the state boundary
+        colorscale = 'Reds',
+        colorbar_title = "Number of Alumni",
+    ))
+    fig.update_layout(
+        title_text = 'UCSD Alumni Employment Localization Distribution',
+        geo_scope='usa', # limite map scope to USA
+    )
+    fig.show()
+
+
+    # Data to plot
+    labels = 'aligned', 'not aligned'
+    sizes = [align, numpeople - align]
+    colors = ['gold', 'lightskyblue']
+    explode = (0.1, 0)  # explode 1st slice
+
+    # plt.axis('equal')
+    label = ['aligned', 'not aligned']
+    total = sum(sizes)
+    number = [i/total for i in sizes]
+    figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
+    colors=['pink','lightskyblue','lightcyan',  
+            'lightcoral', 'lightgreen', 'plum', 'lightgrey', 'lightblue',
+           'limegreen','coral','c','dodgerblue','tomato','lightsalmon']
+    plt.rcParams.update({'font.size': 20})
+    plt.pie(number, labels=label, colors=colors, shadow=False)
+    plt.title("Percentage that UCSD Alumni's jobs aligned with their major")
+    plt.savefig('Percentage that UCSD Alumnis jobs aligned with their major')
+
+
+
+    wc = WordCloud(width = 800, height = 400,background_color ='white').generate_from_frequencies(field_of_study)
+    plt.figure(figsize = (8, 8), facecolor = None)
+    plt.imshow(wc, interpolation='bilinear') 
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+
+    """
+    Draw the line that that shows UCSD incoming student's location
+    """
+
+    incoming = dict()
+    # Date gathered from UCSD Website
+    incoming['San Diego'] = [0.14, 0.11, 0.13, 0.13, 0.12, 0.14]
+    incoming['Los Angeles'] = [0.41, 0.37, 0.38, 0.37, 0.32, 0.36]
+    incoming['San Francisco'] = [0.18, 0.19, 0.16, 0.16, 0.16,0.14]
+    incoming['Other CA'] = [0.1, 0.09, 0.1, 0.09, 0.08, 0.08]
+    incoming['Out-of-state'] = [0.07, 0.1, 0.07, 0.06, 0.08,0.07]
+    incoming['International'] = [0.11, 0.15, 0.16, 0.21, 0.24, 0.22]
+
+    random_x = [2011, 2012, 2013, 2014, 2015, 2016]
+    random_y0 = (incoming['San Diego'])
+    # Create traces
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=random_x, y=incoming['Los Angeles'],
+                        mode='lines+markers',
+                        name='Los Angeles',
+                        line=dict(color='red', width=2)))
+    fig.add_trace(go.Scatter(x=random_x, y=incoming['San Diego'],
+                        mode='lines+markers',
+                        name='San Diego',
+                        line=dict(color='orange', width=2)))
+    fig.add_trace(go.Scatter(x=random_x, y=incoming['San Francisco'],
+                        mode='lines+markers',
+                        name='San Francisco'))
+    # fig.add_trace(go.Scatter(x=random_x, y=incoming['Other CA'],
+    #                     mode='lines+markers',
+    #                     name='Other CA'))
+    # fig.add_trace(go.Scatter(x=random_x, y=incoming['Out-of-state'],
+    #                     mode='lines+markers',
+    #                     name='Out-of-state'))
+    fig.add_trace(go.Scatter(x=random_x, y=incoming['International'],
+                        mode='lines+markers',
+                        name='International',
+                        line=dict(color='royalblue', width=6, dash='dot')))
+    # fig.add_trace(go.Scatter(x=random_x, y=random_y2,
+    #                     mode='markers', name='markers'))
+
+    # Edit the layout
+    fig.update_layout(title='Regional Distribution for incoming UCSD Student',titlefont=dict(color='rgb(255, 100, 0)',size=30),
+                       xaxis_title='Year',
+                       yaxis_title='Percentage')
+    fig.show()
+
+
+
+    """
+    This part is to help the to get the pie chart of the amount of people that have major aligned with their industry
+    """
+    labels = 'aligned', 'not aligned'
+    sizes = [align, numpeople - align]
+    colors = ['gold', 'lightskyblue']
+    explode = (0.1, 0)  # explode 1st slice
+
+    # Plot
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+    autopct='%1.1f%%', shadow=True, startangle=140)
+
+    plt.axis('equal')
 
